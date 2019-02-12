33d359a8b5aa7761fe802080044f5577e37c8d05
jahnisko <vitezslav.jahn@post.cz>
2019-02-12 10:13:31 +0100

    druhy

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Funguje to!");
+
+function decToBin(num){
+    var vysledek = '';
+    do{
+        vysledek = (num%2) + vysledek;
+        num = Math.floor(num / 2);
+    }while(num != 0);
+    return vysledek;
+}
+console.log(decToBin(54));
